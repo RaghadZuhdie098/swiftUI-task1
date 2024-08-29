38d4b862a3f42129d21746cead7c704b550d0ee1
raghad <raghad.zuhdie98@gmail.com>
2024-08-29 07:21:51 +0400

    Initial Commit

---
+++ firstTask/firstTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstTaskApp.swift
+//  firstTask
+//
+//  Created by raghad zuhdie on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct firstTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
